cc7475004eb152c052614af9c5fbffdce7b585d7
Myroslav Kiurchev <kiurchv@gmail.com>
2018-02-18 03:01:04 +0200

    Turn most common aliases into abbreviations

---
+++ conf.d/abbrs.fish
@@ -0,0 +1,43 @@
+abbr -a g git
+abbr -a ga git add
+
+abbr -a gbs git bisect
+
+abbr -a gb git branch
+
+abbr -a gco git checkout
+abbr -a gcb git checkout -b
+
+abbr -a gcp git cherry-pick
+
+abbr -a gcl git clone --recursive
+
+abbr -a gc git commit -v
+abbr -a gc! git commit -v --amend
+abbr -a gcmsg git commit -m
+
+abbr -a gd git diff
+abbr -a gdca git diff --cached
+
+abbr -a gf git fetch
+abbr -a gfo git fetch origin
+
+abbr -a gm git merge
+
+abbr -a gl git pull
+
+abbr -a gp git push
+abbr -a gpd git push --dry-run
+
+abbr -a grb git rebase
+
+abbr -a gr git remote
+
+abbr -a gru git reset --
+abbr -a grh git reset HEAD
+
+abbr -a gsta git stash
+
+abbr -a gst git status
+
+abbr -a gts git tag -s
--- conf.d/aliases.fish
+++ conf.d/aliases.fish
@@ -1,61 +1,42 @@
-alias g 'git'
-alias ga 'git add'
 alias gaa 'git add --all'
 alias gapa 'git add --patch'
 
-alias gb 'git branch'
-alias gba 'git branch -a'
 alias gbl 'git blame -b -w'
-alias gbnm 'git branch --no-merged'
-alias gbr 'git branch --remote'
-alias gbd 'git branch -d'
-alias gbfd 'git branch -D'
-alias gbs 'git bisect'
+
 alias gbsb 'git bisect bad'
 alias gbsg 'git bisect good'
 alias gbsr 'git bisect reset'
 alias gbss 'git bisect start'
 
-alias gc 'git commit -v'
-alias gc! 'git commit -v --amend'
+alias gba 'git branch -a'
+alias gbnm 'git branch --no-merged'
+alias gbr 'git branch --remote'
+alias gbd 'git branch -d'
+alias gbfd 'git branch -D'
+alias ggsup 'git branch --set-upstream-to=origin/(_current_branch)'
+
+alias gcm 'git checkout master'
+alias gcd 'git checkout develop'
+
 alias gca 'git commit -v -a'
 alias gca! 'git commit -v -a --amend'
 alias gcam 'git commit -v -a -m'
 alias gcan! 'git commit -v -a -s --no-edit --amend'
-alias gcb 'git checkout -b'
-alias gcd 'git checkout develop'
-alias gcf 'git config --list'
-alias gcl 'git clone --recursive'
-
-alias gcm 'git checkout master'
-alias gcmsg 'git commit -m'
-alias gco 'git checkout'
-alias gcount 'git shortlog -sn'
-alias gcp 'git cherry-pick'
 alias gcs 'git commit -S'
 
-alias gd 'git diff'
-alias gdca 'git diff --cached'
+alias gcf 'git config --list'
+
 alias gdt 'git diff-tree --no-commit-id --name-only -r'
 alias gdw 'git diff --word-diff'
 
-alias gf 'git fetch'
 alias gfa 'git fetch --all --prune'
-alias gfo 'git fetch origin'
 
 alias gg 'git gui citool'
 alias gga 'git gui citool --amend'
-alias ggpull 'git pull origin (_current_branch)'
-alias ggpush 'git push origin (_current_branch)'
-alias ggsup 'git branch --set-upstream-to=origin/(_current_branch)'
-
-alias gignore 'git update-index --assume-unchanged'
-alias gignored 'git ls-files -v | grep "^[[:lower:]]"'
 
 alias gk 'gitk --all --branches'
 alias gke 'gitk --all (git log -g --pretty=format:%h)'
 
-alias gl 'git pull'
 alias glg 'git log --stat --color'
 alias glgp 'git log --stat --color -p'
 alias glgg 'git log --graph --color'
@@ -66,60 +47,62 @@ alias glol="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset
 alias glola="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
 alias glog 'git log --oneline --decorate --color --graph'
 
-alias gm 'git merge'
+alias gignored 'git ls-files -v | grep "^[[:lower:]]"'
+
 alias gmom 'git merge origin/master'
-alias gmt 'git mergetool --no-prompt'
-alias gmtvim 'git mergetool --no-prompt --tool=vimdiff'
 alias gmum 'git merge upstream/master'
 
-alias gp 'git push'
-alias gpd 'git push --dry-run'
-
-alias gpu 'git push upstream'
-alias gpv 'git push -v'
-
-alias gr 'git remote'
-alias gra 'git remote add'
-alias grb 'git rebase'
-alias grba 'git rebase --abort'
-alias grbc 'git rebase --continue'
-alias grbi 'git rebase -i'
-alias grbm 'git rebase master'
-alias grbs 'git rebase --skip'
-alias grh 'git reset HEAD'
-alias grhh 'git reset HEAD --hard'
-alias grmv 'git remote rename'
-alias grrm 'git remote remove'
-alias grset 'git remote set-url'
-alias grt 'cd (git rev-parse --show-toplevel ; or echo ".")'
-alias gru 'git reset --'
-alias grup 'git remote update'
-alias grv 'git remote -v'
-
-alias gsb 'git status -sb'
-alias gsd 'git svn dcommit'
-alias gsi 'git submodule init'
-alias gsps 'git show --pretty=short --show-signature'
-alias gsr 'git svn rebase'
-alias gss 'git status -s'
-alias gst 'git status'
-alias gsta 'git stash'
-alias gstaa 'git stash apply'
-alias gstd 'git stash drop'
-alias gstl 'git stash list'
-alias gstp 'git stash pop'
-alias gsts 'git stash show --text'
-alias gsu 'git submodule update --init'
-alias gsf 'git submodule foreach'
-
-alias gts 'git tag -s'
-
-alias gunignore 'git update-index --no-assume-unchanged'
+alias gmt 'git mergetool --no-prompt'
+alias gmtvim 'git mergetool --no-prompt --tool=vimdiff'
 
 alias gup 'git pull --rebase'
 alias gupv 'git pull --rebase -v'
+alias ggpull 'git pull origin (_current_branch)'
+
+alias gpv 'git push -v'
+alias gpu 'git push upstream'
+alias ggpush 'git push origin (_current_branch)'
+
+alias grbi 'git rebase -i'
+alias grba 'git rebase --abort'
+alias grbc 'git rebase --continue'
+alias grbs 'git rebase --skip'
+alias grbm 'git rebase master'
+
+alias grv 'git remote -v'
+alias gra 'git remote add'
+alias grmv 'git remote rename'
+alias grup 'git remote update'
+alias grset 'git remote set-url'
+alias grrm 'git remote remove'
+
+alias grhh 'git reset HEAD --hard'
+
+alias grt 'cd (git rev-parse --show-toplevel ; or echo ".")'
+
+alias gcount 'git shortlog -sn'
+
+alias gsps 'git show --pretty=short --show-signature'
+
+alias gstl 'git stash list'
+alias gsts 'git stash show --text'
+alias gstaa 'git stash apply'
+alias gstp 'git stash pop'
+alias gstd 'git stash drop'
+
+alias gss 'git status -s'
+alias gsb 'git status -sb'
+
+alias gsi 'git submodule init'
+alias gsu 'git submodule update --init'
+alias gsf 'git submodule foreach'
+
+alias gsd 'git svn dcommit'
+alias gsr 'git svn rebase'
+
+alias gignore 'git update-index --assume-unchanged'
+alias gunignore 'git update-index --no-assume-unchanged'
 
 alias gvt 'git verify-tag'
 
 alias gwch 'git whatchanged -p --abbrev-commit --pretty=medium'
-alias gwip 'git add -A; git rm (git ls-files --deleted) 2> /dev/null; git commit -m "--wip--"'
